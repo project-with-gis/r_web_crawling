fc60710af2a8c78c6966179a55946e4d45e39436
aj8782 <dkwls8782@likelion.org>
2021-11-03 17:12:18 +0900

    our project

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+from diningcode_api import *
+from csv_handler import *
+from siksin_api import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def main(path):
+    # store_info 파일 읽어오는 함수 실행
+    info_df = read_csv(path)
+
+    # 다이닝코드 리뷰 크롤링 함수 실행
+    link_df = diningcode_link(info_df)
+    review_df_da = diningcode_review(link_df)
+
+    # 식신 리뷰 크롤링 함수 실행
+    store_df = add_siksin_info(info_df)
+    review_df_si = siksin_review_scraping(store_df)
+
+    # 구글 리뷰 크롤링 함수 실행
+    # 네이버 리뷰 크롤링 함수 실행
+    # 사이트4개 리뷰 합치기
+    total_review = concat_df(review_df_si, review_df_da)
+    # review 파일에 전처리 컬럼 추가
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    # csv 파일로 저장
+    save_csv(total_review, path, name)
+
+    return
 
 
-# Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
