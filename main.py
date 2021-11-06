94c403cc528fbd4fb3bc520040c3d262a5e5dadf
aj8782 <dkwls8782@naver.com>
2021-11-06 16:42:55 +0900

    def google_crawling(path) 추가

--- main.py
+++ main.py
@@ -4,6 +4,30 @@ from crawler_api.siksin_api import *
 from crawler_api.google_api import *
 from crawler_api.naver_api import *
 
+# 구글 리뷰 크롤링해서 저장하는 함수
+def google_crawling(path):
+    # store_info 파일 읽어오는 함수 실행
+    info_df = read_csv(path)
+
+    # 특정 가게만 지정할 때
+    # info_df = info[1800:1900].reset_index(drop=True)
+
+    # 리뷰데이터 크롤링
+    storeInfo, review_df_go = google(info_df)
+
+    # 영어리뷰 번역리뷰 제거
+    review_df_go = google_eng_transfer_del(review_df_go)
+
+    # csv 파일로 저장
+    # save_csv(review_df_goo, path, name)
+
+    return review_df_go
+
+
+
+
+
+
 def main(path):
     # store_info 파일 읽어오는 함수 실행
     info_df = read_csv(path)
